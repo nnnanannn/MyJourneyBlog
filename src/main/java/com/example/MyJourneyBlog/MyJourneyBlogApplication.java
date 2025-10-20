0c6b823a2ec4b490a212ce50758a6b29d1cb980e
nnnanannn <yuttitham_ning@hotmail.com>
2025-10-20 21:27:13 +0900

    Initial commit

---
+++ src/main/java/com/example/MyJourneyBlog/MyJourneyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyJourneyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJourneyBlogApplication.class, args);
+	}
+
+}
