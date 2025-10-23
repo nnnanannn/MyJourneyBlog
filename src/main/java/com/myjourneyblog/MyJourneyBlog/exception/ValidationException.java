6ca1911bf325726d4c71883a65f9ce579ebb40bd
nnnanannn <yuttitham_ning@hotmail.com>
2025-10-23 21:33:51 +0900

    feat: Create custom exception classes - Add ValidationException for business rule violations

---
+++ src/main/java/com/myjourneyblog/MyJourneyBlog/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.myjourneyblog.MyJourneyBlog.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
