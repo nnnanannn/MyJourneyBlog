cddb1d5f1e31bd4fd90f0ae20be5112d977a0bf9
nnnanannn <yuttitham_ning@hotmail.com>
2025-10-28 23:39:17 +0900

    feat: Create enum types for entities
    - Add ProjectStatus enum (PLANNING to CANCELLED)
    - Add UpdateType enum (FEATURE, BUG_FIX, etc.)
    - Will use @Enumerated(EnumType.STRING) for database storage

---
+++ src/main/java/com/myjourneyblog/MyJourneyBlog/model/ProjectStatus.java
@@ -0,0 +1,13 @@
+package com.myjourneyblog.MyJourneyBlog.model;
+
+/**
+ * Status of project updates
+ */
+public enum ProjectStatus {
+    PLANNING,
+    IN_PROGRESS,
+    TESTING,
+    COMPLETED,
+    ON_HOLD,
+    CANCELLED
+}
---
+++ src/main/java/com/myjourneyblog/MyJourneyBlog/model/UpdateType.java
@@ -0,0 +1,14 @@
+package com.myjourneyblog.MyJourneyBlog.model;
+
+/**
+ * Types of project updates
+ */
+public enum UpdateType {
+    FEATURE,
+    BUG_FIX,
+    REFACTOR,
+    DOCUMENTATION,
+    TESTING,
+    DEPLOYMENT,
+    LEARNING
+}
